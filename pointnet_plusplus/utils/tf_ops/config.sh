e15e46ddd1779bd357698c2f679669d70e45bda3
Phillip Miao <pmiao@stanford.edu>
2023-05-12 17:47:59 -0700

    Update config.sh

--- pointnet_plusplus/utils/tf_ops/config.sh
+++ pointnet_plusplus/utils/tf_ops/config.sh
@@ -3,9 +3,9 @@
 nvcc_bin=/usr/local/cuda/bin/nvcc
 
 cuda_include_dir=/usr/local/cuda/include
-tensorflow_include_dir=/home/ubuntu/miniconda3/envs/cs231n/lib/python3.6/site-packages/tensorflow/include
-tensorflow_external_dir=/home/ubuntu/miniconda3/envs/cs231n/lib/python3.6/site-packages/tensorflow/include/external/nsync/public
+tensorflow_include_dir=/home/ubuntu/miniconda3/envs/cs231n/lib/python3.8/site-packages/tensorflow/include
+tensorflow_external_dir=/home/ubuntu/miniconda3/envs/cs231n/lib/python3.8/site-packages/tensorflow/include/external/nsync/public
 
 cuda_library_dir=/usr/local/cuda/lib64/
-tensorflow_library_dir=/home/ubuntu/miniconda3/envs/cs231n/lib/python3.6/site-packages/tensorflow
+tensorflow_library_dir=/home/ubuntu/miniconda3/envs/cs231n/lib/python3.8/site-packages/tensorflow
 
